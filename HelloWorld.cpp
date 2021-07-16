ed38a5a8ae3be12f95ef33cc1ea426fb46a776e9
Darshan Parulekar <parulekardarshan29@gmail.com>
2021-07-16 19:02:03 +0530

    Add files via upload
    Simple Hello World Program Using C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream.h> //Basic i/o service for c++
+#include<conio.h> //Console Input-Output
+
+void main()
+{
+	clrscr(); //clears screen
+	cout<<"Hello World";
+	getch(); //pauses the output console untill a key is pressed
+}
